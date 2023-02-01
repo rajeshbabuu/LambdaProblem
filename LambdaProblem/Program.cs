efa5810a8c4e7b97fd666a8820ee3380175d4976
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-02-01 22:42:21 +0530

    Lambda Expressions

---
+++ LambdaProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LambdaProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<Person> listPersonlnCity = new List<Person>();
+            LambdaDemo.AddRecords(listPersonlnCity);
+
+            LambdaDemo.Retrieving_TopTwoRecord_ForAgels_LessThanSixty(listPersonlnCity);
+            LambdaDemo.CheckingForTeenagerPerson(listPersonlnCity);
+            LambdaDemo.AllPersonsAverageAge(listPersonlnCity);
+            LambdaDemo.CheckNameExistOrNot(listPersonlnCity);
+            Console.ReadKey();
+        }
+    }
+}
